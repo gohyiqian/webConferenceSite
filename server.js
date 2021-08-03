b55ee1d0ae4a17892320425798315002343a8997
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-03 11:44:05 +0800

    update

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static(path.join(__dirname,'./static')))
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,'./static/index.html'));
+});
+
+app.get("/speakers", (req, res) => {
+  res.sendFile(path.join(__dirname,'./static/speakers.html'));
 });
 
 app.listen(port, () => {
