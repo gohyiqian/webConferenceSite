1012618e2181b515f0284cad47e61959a3d4fed7
gyq765 <kenji.gohyiqian@gmail.com>
2021-08-03 10:46:18 +0800

    Index

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webConferenceSite
 Built with node and express
+
+A simple website for an art conference showing agenda, event highlights andd speakers lists.
+
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.sendFile(path.join(__dirname,'./static/index.html'));
 });
 
 app.listen(port, () => {
